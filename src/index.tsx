efe95056249bd3b2952c36655847ced4cbc3c32e
Ivan <vanna7417@gmail.com>
2023-12-11 19:37:36 +0100

    created component

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,25 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 // const el = React.createElement('h1', { id: 'title' }, 'Hello title')
 
 //with jsx
-const a = 10
-const el = <h1 id="title">Hello Title, {a}</h1>
-root.render(<React.StrictMode>{el}</React.StrictMode>)
+// const a = 10
+// const el = <h1 id="title">Hello Title, {a}</h1>
+
+function App() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Numquam, cupiditate perferendis sed culpa ut quaerat voluptatum,
+                ex est doloribus ipsum laborum explicabo a eaque minus, ullam
+                beatae. Ab, cum natus.
+            </p>
+        </div>
+    )
+}
+root.render(
+    <React.StrictMode>
+        {' '}
+        <App />{' '}
+    </React.StrictMode>
+)
