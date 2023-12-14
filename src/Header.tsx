571695a054dd2d6f037fe893cecd2d853f2ae0d7
Ivan <vanna7417@gmail.com>
2023-12-14 18:52:48 +0100

    worked with class

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,20 +1,17 @@
-const Header = ({ title, num, bgColor, position }: HeaderType) => {
-    const style = {
-        backgroundColor: bgColor,
-        color: position ? 'white' : 'black',
-        padding: '15px',
-    }
-    return (
-        <h1 style={style}>
-            Hello world , {title}, {num}
-        </h1>
-    )
-}
+import './Header.css'
+import clsx from 'clsx'
+
 type HeaderType = {
     title: string
     num?: number
-    bgColor: string
-    position?: boolean
+}
+
+const Header = ({ title, num }: HeaderType) => {
+    return (
+        <h1 className={`title ${num === 10 ? `red` : ``}`}>
+            Hello world , {title}, {num}
+        </h1>
+    )
 }
 
 export default Header
--- src/index.tsx
+++ src/index.tsx
@@ -27,8 +27,8 @@ const Content = () => {
 const App = () => {
     return (
         <>
-            <Header title="App.js" num={10} bgColor="blue" position />
-            <Header title="React.js" bgColor="purple" />
+            <Header title="App.js" num={10} />
+            <Header title="React.js" />
             <Content />
         </>
     )
