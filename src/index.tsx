825a4f975781d2f24f5cd1f435c03af39c47a881
Ivan <vanna7417@gmail.com>
2023-12-18 18:31:43 +0100

    deleted header. content

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Header from './Header'
-import Content from './Content'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 const App = () => {
-    return (
-        <>
-            <Header title="App.js" num={10} />
-            <Header title="React.js" />
-            <Content p1="Hello World 1" p2="Hello World 2" bgColor="red" />
-            <Content p1="Hello World 3" p2="Hello World 4" bgColor="purple" />
-        </>
-    )
+    return <></>
 }
 root.render(
     <React.StrictMode>
