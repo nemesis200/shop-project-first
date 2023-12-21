c4bc3061eb05e826ca9896d975eb0e4dab4cf350
Ivan <vanna7417@gmail.com>
2023-12-21 18:51:31 +0100

    added Props

--- src/components/Logo/Products/ProductList.tsx
+++ src/components/Logo/Products/ProductList.tsx
@@ -16,19 +16,33 @@ const ProductList = (props: Props) => {
             </Typography>
             <Grid container spacing={4}>
                 <Grid item xs={12} sm={6} md={4}>
-                    <PaginationItem />
+                    <PaginationItem
+                        title="iPhone 15 Pro"
+                        description="This is iPhohe 15 Pro"
+                        capacity="256"
+                        type="phone"
+                        price={1500}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <PaginationItem />
+                    <PaginationItem
+                        title="iPhone 4 Pro"
+                        description="This is iPhohe 14 Pro"
+                        capacity="128"
+                        type="phone"
+                        price={1000}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <PaginationItem />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <PaginationItem />
+                    <PaginationItem
+                        title="iPhone 13 Pro"
+                        description="This is iPhohe 13 Pro"
+                        capacity="64"
+                        type="phone"
+                        price={500}
+                    />
                 </Grid>
             </Grid>
-            <PaginationItem />
         </>
     )
 }
