5bd9e8a598f371c23f4cbbd0a6a1df792f3d9650
Ivan <vanna7417@gmail.com>
2024-01-11 19:43:55 +0100

    added Reviews

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,10 +1,12 @@
 import ProductList from 'components/Products/ProductList'
+import Reviewes from 'components/Reviewes/Reviewes'
 type Props = { addProductToCart: (id: number, count: number) => void }
 
 const Home = ({ addProductToCart }: Props) => {
     return (
         <>
             <ProductList addProductToCart={addProductToCart} />
+            <Reviewes />
         </>
     )
 }
